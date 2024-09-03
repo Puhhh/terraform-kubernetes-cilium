f98ebbc5b6ddc90b27a31a78dc199c9663c87ecd
Aleksei Blinov <alvablinov@gmail.com>
2024-09-03 16:54:13 +0300

    tflint issues fix

--- cilium.tf
+++ cilium.tf
@@ -10,5 +10,5 @@ resource "helm_release" "cilium" {
   repository       = var.helm-chart-repo
   version          = var.helm-chart-version
 
-  values = var.helm-custom-values ? [file("${var.helm-custom-values-path}")] : []
+  values = var.helm-custom-values ? [file(var.helm-custom-values-path)] : []
 }
